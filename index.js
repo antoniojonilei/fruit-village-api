aa010ad68ceb8ee486cf87493e5a06cdb86db18b
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-06 10:30:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
 const fruits = require("./src/fruits/fruits.json")
+
+app.use(cors())
 
 app.get('/fruits', function(req, res) {
     return res.json(fruits)
